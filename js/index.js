ae54ed07d691d19b64e176c2e78626dd33c45831
Sudhanshu Mishra <mrsud94@gmail.com>
2013-10-22 06:12:48 +0530

    added colors

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $( document ).ready(function (){
-	
+/*	
 	$("#immersive_slider").immersive_slider({
 	  animation: "slide", // As usual, you can change the animation to these: slide (default), bounce, fade, slideUp, and bounceUp
 	  slideSelector: ".slide", // This option will let you assign custom selector for each slides in case .slide is already taken
@@ -9,11 +9,11 @@ $( document ).ready(function (){
 	  loop: true, // Toggle to false if you don't want the slider to loop. Default is true.
 	  autoStart: 0 // Define the number of milliseconds before it navigates automatically. Change this to 0 or false to disable autoStart. The default value is 5000.
 	});
-
+*/
 	$(".head").onepage_scroll({
 	   sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
-	   easing: "ease-in-out", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-	   animationTime: 800, // AnimationTime let you define how long each section takes to animate
+	   easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+	   animationTime: 500, // AnimationTime let you define how long each section takes to animate
 	   pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
 	   updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 	   beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
